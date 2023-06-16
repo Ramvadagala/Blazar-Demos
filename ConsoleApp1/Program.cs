d29d72082ef08c175314f310ad615f98abedbef5
Veeraiah.Vadgula <Veeraiah.Vadgula@op.fi>
2023-06-16 12:33:02 +0530

    Add existing project files prior to the push to GitHub.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(10, 20);
+            Console.Read();
+        }
+        public static int Sum(int num1, int num2)
+        {
+            try
+            {
+                int total;
+                total = num1 + num2;
+                Console.WriteLine(total);
+                return total;
+            }
+            catch (Exception ex)
+            {
+                return int.Parse(" 404 Bad Request");
+            }
+        }
+    }
+}
+
